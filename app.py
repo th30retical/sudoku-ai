a18f2ae408f7637d3a9f227b58eb76d14dd31421
theodoretan <tfjtan@hotmail.com>
2017-05-27 21:41:08 -0400

    should connect to port now

--- app.py
+++ app.py
@@ -9,4 +9,6 @@ api = Api(app)
 api.add_resource(sudokuSolver.SudokuSolver, '/solve')
 
 if __name__ == '__main__':
-	app.run()
+	# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
